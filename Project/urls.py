9071f90120ea4eac251629413916da8c450ad7f4
Joseph Rios Henao <joseph232005@gmail.com>
2022-03-13 02:20:59 -0500

    fix

--- Project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include("apps.home.urls"), name="home"),
-    path('register', include("apps.register.urls"), name="register"),
-    path('login', include("apps.login.urls"), name="login"),
-    path('admin/', admin.site.urls),
-]
