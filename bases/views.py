8923a005da1785f9f125f2302672bcd98b7ec5c7
Joseph Rios Henao <joseph232005@gmail.com>
2022-03-13 14:01:54 -0500

    update

---
+++ bases/views.py
@@ -0,0 +1,17 @@
+from pipes import Template
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+# Create your views here.
+
+class Home(LoginRequiredMixin, TemplateView): 
+    template_name = "bases/home.html"
+    login_url = 'bases:login'
+
+
+class Login(TemplateView): 
+    template_name = "bases/login.html"
