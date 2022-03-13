55b3a06741db3f7d5b051bad19d4f1be76e74214
Joseph Rios Henao <joseph232005@gmail.com>
2022-03-13 02:24:38 -0500

    fix

--- apps/home/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, ListView, TemplateView
-# Create your views here.
-class Home(TemplateView):
-    template_name = "home/index.html"
