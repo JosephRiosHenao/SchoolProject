1ffe2228e5a0997db124308bc298c5cd9187c8cb
Joseph Rios Henao <joseph232005@gmail.com>
2022-03-14 17:01:43 -0500

    save model generic

--- bases/models.py
+++ bases/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class ClassModel(models.Model):
+    state = models.BooleanField(default=True),
+    create_date = models.DateTimeField(auto_now_add=True),
+    modified_date = models.DateTimeField(auto_now=True),
+    user_created = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_modified = models.ForeignKey(User, on_delete=models.CASCADE)
