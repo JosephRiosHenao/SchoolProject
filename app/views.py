dbf5b0174e5decc5151d020942b1175941229818
Joseph Rios <administrador@Josephs-MacBook-Pro.local>
2022-06-05 02:35:50 -0500

    fix

--- app/views.py
+++ app/views.py
@@ -2,6 +2,23 @@ from django.views.generic import TemplateView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from cmp.models import *
+
+from inventory.models import *
+
+from django.db.models import Sum
+
+from todo.models import *
+
 class Home(LoginRequiredMixin, TemplateView):
     template_name = 'app/home.html'
     login_url = 'users:login'
+    
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['total'] = BuyHead.objects.all().aggregate(Sum('total'))['total__sum']
+        context['vendidos'] = BuyData.objects.all().aggregate(Sum('stock'))['stock__sum']
+        context['productos'] = Product.objects.all().count()
+        context['categorias'] = Category.objects.all().count()
+        context['tareas'] = Task.objects.all().count()
+        return context
